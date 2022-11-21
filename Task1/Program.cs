fc0b06d2ce9fce464268f9e9710f23e673113196
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-21 16:11:36 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:/Temp";
+            string[] files = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
